c7779b1b338ad5ccb74ab738869d2ffa504129e4
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-04 18:59:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,31 +5,9 @@ client.on('ready', () => {
     client.channels.find(c => c.id === '496332757765718036').join();
 });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`- Esinti ..`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+    client.on('ready', () => {
+     client.user.setActivity("- Esinti .",{type: 'Listening'});
 
+});
 
 client.login(process.env.BOT_TOKEN);
