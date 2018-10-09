07324b5cb242b5facdc4d14b4199b266bbd917c7
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-09 18:08:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,12 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.channels.find(c => c.id === '496333620580057098').join();
+    client.channels.find(c => c.id === '498285418480009216').join();
 });
 
-client.on('ready', () => {
-    client.channels.find(c => c.id === '491674187870502944').join();
-});
 
     client.on('ready', () => {
-     client.user.setActivity("- وآحـد مقهورر من [رينآد] .ء",{type: 'Listening'});
+     client.user.setActivity("I Hope Go to US . ❤",{type: 'Listening'});
 
 });
 
