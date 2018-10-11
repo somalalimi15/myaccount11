89a59f5514d0702452d2fa09c2a579c6945ea896
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-12 01:50:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.channels.find(c => c.id === '498488191750242314').join();
+    client.channels.find(c => c.id === '499649811151061002').join();
 });
 
 
     client.on('ready', () => {
-     client.user.setActivity("I Hope Go To US . ❤",{type: 'Listening'});
+     client.user.setActivity("I Hope To Go To US . ❤",{type: 'Listening'});
 
 });
 
