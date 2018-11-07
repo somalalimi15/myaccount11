71f6f307280342d893c95229c8d59866d9d793c3
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-07 17:16:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,28 @@
 const client = new Discord.Client();
 const Discord = require('discord.js'),
       
-client.on('message', message => {
-    if(message.content.startsWith(`hi`)){
-        message.guild.channels.deleteAll();
+client.on("message", (message) => {
+    if(!message.channel.guild) return;
+     let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+if (command == "cv") {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+message.channel.sendMessage('تم انشاء روم صوتى')
+}
+});
+client.on("message", (message) => {
+    let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+if (command == "حذف") {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(' ').slice(1);
+        let channel = message.client.channels.find('name', args.join(' '));
+        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
+        channel.delete()
     }
 });
+
 client.login(process.env.BOT_TOKEN); 
 
