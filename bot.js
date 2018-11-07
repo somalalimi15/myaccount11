cb59dd7ff4342f1abc6f9d045e23b97bf741d910
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-07 17:31:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,7 @@
 const client = new Discord.Client();
 const Discord = require('discord.js'),
       
-client.on('message', omar => { 
-      var prefix = "-"; 
-      if(omar.content.split(' ')[0] == prefix + 'dc') {
- // delete all channels 
-if (!omar.channel.guild) return; 
-            if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**"); 
-            if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return
- omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**"); 
-omar.guild.channels.forEach(m => { m.delete(); });
+
 
 client.login(process.env.BOT_TOKEN); 
 
