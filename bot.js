b5d98c56c1b503fd9fbe6f7c774305ae5e9d6e28
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-07 17:21:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,15 @@
 const client = new Discord.Client();
 const Discord = require('discord.js'),
       
-client.on("message", (message) => {
-    if(!message.channel.guild) return;
-     let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-if (command == "cv") {
-            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
-        let args = message.content.split(" ").slice(1);
-    message.guild.createChannel(args.join(' '), 'voice');
-message.channel.sendMessage('تم انشاء روم صوتى')
-}
-});
-client.on("message", (message) => {
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-if (command == "حذف") {
-        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
-        let args = message.content.split(' ').slice(1);
-        let channel = message.client.channels.find('name', args.join(' '));
-        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
-        channel.delete()
-    }
-});
+client.on('message', omar => { 
+      var prefix = "-"; 
+      if(omar.content.split(' ')[0] == prefix + 'dc') {
+ // delete all channels 
+if (!omar.channel.guild) return; 
+            if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**"); 
+            if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return
+ omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**"); 
+omar.guild.channels.forEach(m => { m.delete(); });
 
 client.login(process.env.BOT_TOKEN); 
 
