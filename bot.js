77b9a57a3b2d5829733b04810d77b2d765f76859
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-08 22:36:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
 const client = new Discord.Client();
 const Discord = require('discord.js'),
       
-
+client.on('guildMemberAdd', Sal => { //! Ayman ᴸᶜ#9955
+    var embed = new Discord.RichEmbed()
+    .setTitle('Welcome')
+    .setDescription('- Welcome To, Orexn .')
+    .setColor('RANDOM')
+    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+    var channel =Sal.guild.channels.find('name','orexn') // هنا حط اسم الروم الي تبيه يكتب فيه
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
 
 client.login(process.env.BOT_TOKEN); 
 
